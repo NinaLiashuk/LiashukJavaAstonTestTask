91e675d1a6ce7b0cde800dad7b74708b28ca0d42
NinaLiashuk <la-nina84@mail.ru>
2022-10-05 13:03:45 +0300

    done

---
+++ src/main/java/task/Runner.java
@@ -0,0 +1,10 @@
+package task;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Hello1.sayHello();
+        Hello2.sayHello();
+        Array.getNumbersMultipleThree();
+    }
+}
